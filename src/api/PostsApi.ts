74e165db854fbd8077a23fe1ba80427acd6ae556
cihaisun <cihai.sun@bitmart.com>
2020-07-30 22:36:17 -0400

    refactor: optimize code

--- src/api/PostsApi.ts
+++ src/api/PostsApi.ts
@@ -1,13 +1,4 @@
-import { Post } from '../models/Post';
 import axios from 'axios';
 import configuration from '../constants/config';
 
-export const getPosts = () => {
-    return axios.get(`${configuration.domain}/get-posts`);
-    // return axios({
-    //     method: 'GET',
-    //     url: `${configuration.domain}/get-posts`,
-    //     withCredentials: true,
-    //     headers: { 'Access-Control-Allow-Origin': '*' },
-    // });
-};
+export const getPosts = () => axios.get(`${configuration.domain}/get-posts`);
